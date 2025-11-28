cd0485e43d5f252aae92250b1c719fcee55b34f0
Mor992 <152429764+Mor992@users.noreply.github.com>
2025-11-28 23:34:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.title("Skin Lesion Classifier")
 def load_model():
     # Download if missing
     if not os.path.exists(MODEL_PATH):
-        st.info("Downloading model...")
+        
         url = f"https://drive.google.com/uc?id={MODEL_DRIVE_ID}"
         gdown.download(url, MODEL_PATH, quiet=False)
     
@@ -52,7 +52,11 @@ if uploaded_file:
     img_array = np.expand_dims(img_array, 0)
 
     # Predict
-    
+    class_names = [
+        "Melanoma → Cancer",
+        "Melanocytic Nevus",
+        "Basal Cell Carcinoma → Cancer",
+        "Actinic Keratosis → Pre-cancerous"]
 
     pred = model.predict(img_array)[0]
     idx = int(np.argmax(pred))
